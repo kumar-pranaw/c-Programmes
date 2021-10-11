a704e6739ea8dd0821672d94aaff5c52883e0eb3
LAPTOP-VTPM4OPO\PK <prnvkmr954@gmail.com>
2021-10-11 08:12:13 +0530

    Initial project setup

---
+++ DSPrograms/SumOfTwoumbers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SumOfTwoumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # c-Programmes
 This repository is dedicated to the c# DS programms 
+# How to run the project
+Open .sln file in visual studio
